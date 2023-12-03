eab9ed84f9d945a74bb87fad53152594608b9746
Dipak <mr.dipaknrathod@gmail.com>
2023-12-03 23:37:20 +0530

    assignment upadated

---
+++ Question_31.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+import java.applet.Applet;
+public class Question_31 extends Applet {
+
+    public void init(){
+        
+
+    }
+
+    public void paint(Graphics g){
+
+    }
+}
